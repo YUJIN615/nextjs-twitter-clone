6dfc2a7983d75c71422b9f07d54316d7dfdee5ed
yujin <rosepark306@gmail.com>
2024-04-09 00:04:01 +0900

    add: 탐색하기 페이지 추가 update: 탐색하기 페이질 때 레이아웃 분기처리

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,8 +1,26 @@
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+import style from './explore.module.css'
 
-export default function Home() {
+export default function Page() {
   return (
-    <div>
-      explore
-    </div>
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
   );
 }
