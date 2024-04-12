d08b2dad8551d894e36d113b37a320142aa1d850
yujin <rosepark306@gmail.com>
2024-04-13 01:49:20 +0900

    add: 다중 이미지 컴포넌트 추가

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
 import {faker} from "@faker-js/faker";
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -26,10 +27,43 @@ export default function Post({ noImage }: Props) {
     createdAt: new Date(),
     Images: [] as any[],
   }
-  if (Math.random() > 0.5 && !noImage) {
+  if (Math.random() < 0.2 && !noImage) {
     target.Images.push({
       imageId: 1,
-      link: faker.image.url()
+      link: faker.image.urlLoremFlickr()
+    })
+  } else if (Math.random() < 0.4 && !noImage) {
+    target.Images.push({
+      imageId: 1,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 2,
+      link: faker.image.urlLoremFlickr()
+    })
+  } else if (Math.random() < 0.6 && !noImage) {
+    target.Images.push({
+      imageId: 1,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 2,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 3,
+      link: faker.image.urlLoremFlickr()
+    })
+  } else if (Math.random() < 0.8 && !noImage) {
+    target.Images.push({
+      imageId: 1,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 2,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 3,
+      link: faker.image.urlLoremFlickr()
+    },{
+      imageId: 4,
+      link: faker.image.urlLoremFlickr()
     })
   }
   return (
@@ -55,13 +89,7 @@ export default function Post({ noImage }: Props) {
           </div>
           <div>{target.content}</div>
           <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}>
-                <img src={target.Images[0].link} alt="" />
-              </Link>
-            )}
+            <PostImages post={target} />
           </div>
           <ActionButtons/>
         </div>
