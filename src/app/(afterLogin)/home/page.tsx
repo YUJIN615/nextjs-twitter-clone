27e8b90dde30912ba0cbd4eedb06350283bfd1c7
yujin <rosepark306@gmail.com>
2024-07-20 02:41:35 +0900

    update: 로그인한 회원 session 전달

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,5 @@
 import {Suspense} from "react";
-import {HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import {auth} from "@/auth";
 import Tab from './_component/Tab'
 import PostForm from './_component/PostForm'
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
@@ -7,14 +7,13 @@ import TabDeciderSuspense from "@/app/(afterLogin)/home/_component/TabDeciderSus
 import Loading from "@/app/(afterLogin)/home/loading";
 import style from './home.module.css'
 
-export default async function Page() {
-
-
+export default async function Home() {
+  const session = await auth();
   return (
     <main className={style.main}>
         <TabProvider>
           <Tab />
-          <PostForm />
+          <PostForm me={session} />
           <Suspense fallback={<Loading />} >
             <TabDeciderSuspense />
           </Suspense>
